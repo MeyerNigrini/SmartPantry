b915864c054a56ac28e9acdf2165454417277788
meyernigrini <meyer.nigrini@1nebula.com>
2025-06-03 13:07:44 +0200

    Restructure backend to conform to onion architecture

---
+++ SmartPantry-backend/3-API/SmartPantry.WebApi/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartPantry.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Hello, SmartPantry!");
+    }
+}
