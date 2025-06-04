b5dd97ffa666d1e54cf846ea7ecb759826783bbd
meyernigrini <meyer.nigrini@1nebula.com>
2025-06-04 14:11:01 +0200

    Create basic DBContext setup and connect to SSMS

---
+++ SmartPantry-backend/1-Shared/SmartPantry.Core/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmartPantry.Core.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }  // Primary Key
+
+        public string Email { get; set; }  // Should be unique
+
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ SmartPantry-backend/2-DataAccess/SmartPantry.DataAccess/Contexts/SmartPantryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartPantry.Core.Entities;
+
+namespace SmartPantry.DataAccess.Contexts
+{
+    public class SmartPantryDbContext : DbContext
+    {
+        public SmartPantryDbContext(DbContextOptions<SmartPantryDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
