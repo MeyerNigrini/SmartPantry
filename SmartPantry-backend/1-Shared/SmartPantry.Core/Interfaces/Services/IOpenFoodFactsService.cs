004a21944a416980df67abdaa39c3f339f3ca3ba
meyernigrini <meyer.nigrini@1nebula.com>
2025-10-29 11:50:58 +0200

    Remove deprecated OpenFoodFacts service and controller
    - Deleted OpenFoodFactsService and related controller endpoints

--- SmartPantry-backend/1-Shared/SmartPantry.Core/Interfaces/Services/IOpenFoodFactsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using SmartPantry.Core.DTOs.OpenFoodFacts;
-
-namespace SmartPantry.Core.Interfaces.Services
-{
-    public interface IOpenFoodFactsService
-    {
-        Task<ProductDetailsResponseDTO> GetProductDetailsByBarcodeAsync(string barcode);
-    }
-}
